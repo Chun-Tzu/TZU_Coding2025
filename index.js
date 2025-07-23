a93bac4ef1976f8e28e8734f88898d73cf1253d7
chun-tzu <bu1222bu@gmail.com>
2025-07-23 17:09:10 +0800

    add js

---
+++ index.js
@@ -0,0 +1,106 @@
+console.log("Hellow")
+0.5
+1
+/*
+// ;("use strict")
+;("jhhjgj")
+true
+false
+
+let a = 1
+// const name = "jac"
+number = 2
+const temp = number
+console.log(temp)
+
+*/
+
+/*
+let a = 10
+let b =20
+let c = a; 
+a = b;     
+b = c;     
+
+console.log(a,b);
+*/
+
+/*
+// 邏輯運算子
+let d = 10
+let e =20
+console.log(d == 10)  //== 檢查兩邊是否相等
+console.log(d != 10)  //!= 檢查兩邊是否不相等
+console.log(d == 10 && e == 20)  //&&  檢查兩邊是否都為真 才顯示True
+console.log(d == 10 || e == 10)  //||  檢查兩邊是否至少一個為真 就顯示True
+
+// 算數運算子
+// + - * / %
+console.log(1+2)
+console.log(2-1)
+console.log(2*3)
+console.log(6/2)
+console.log(5%2)  //餘數
+
+*/
+
+/*
+let a=30
+if(a == 10){
+    console.log("a = 10")
+}else if(a == 15){
+    console.log("a = 15")
+}else{
+    console.log("a不等於10，也不等於15")
+}
+*/
+
+
+/*
+for (let i = 0; i< 10; i+=2){
+    console.log(i)
+}
+console.log("迴圈結束")
+
+
+for (let y = 15; y> 10; y--){
+    console.log(y)
+}
+console.log("迴圈結束")
+
+let double = function (num){
+    return num * 2
+}
+let c = double(2)
+console.log(c)
+*/
+
+/*
+let double = function (num1,num2){
+    return num1 * 2
+}
+let c = double(2,3)  //4
+console.log(c)
+
+let sayMyName =function
+*/
+
+/*
+for (let i = 0; i< 101; i+=2){
+    console.log(i)
+}
+console.log("迴圈結束")
+*/
+
+
+let x = 0
+for (let i = 0; i< 101; i++){
+    x = x + i;
+}
+console.log(x)
+
+
+
+
+
+
